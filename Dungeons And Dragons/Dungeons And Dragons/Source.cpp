71c164d77a6297ca23135d34384a0d03543ee855
Teodor <teodor.karakashev@gmail.com>
2022-05-29 00:38:12 +0300

    Added skeleton

--- Dungeons And Dragons/Dungeons And Dragons/Source.cpp
+++ Dungeons And Dragons/Dungeons And Dragons/Source.cpp
@@ -1,6 +1,8 @@
-#include "ArrayList.hpp"
-#include "String.h"
+#include "Helpers/ArrayList/ArrayList.hpp"
+#include "Helpers/String/String.h"
 
 int main() {
-	
+	ArrayList<int> arr;
+	arr.add(1);
+	std::cout << arr[0];
 }
